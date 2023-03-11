9f7b8af49d3356d8b83285b11babf68019530834
John Wroge <72668920+johnwroge@users.noreply.github.com>
2023-03-10 21:05:03 -0500

    added

--- go-bag/src/App.tsx
+++ go-bag/src/App.tsx
@@ -5,45 +5,42 @@ import './App.css'
 
 // fetch and display with functional components 
 
-function App() {
-
-  interface card {
-    name: string;
-    height: number;
-    mass: number;
-  }
-
-  const [characters, setCharacters] = useState<card[]>([])
-
-  useEffect(() => {
-    fetch("https://swapi.dev/api/people")
-    .then(res => res.json())
-    .then(char => {
-      // console.log(char.results)
-      setCharacters(char.results)
-    })
-    .catch(err => console.log(err))
-  },[]) 
-// console.log(characters)
-//can access array index from second parameter of map function
-const characterList = characters.map((char,index) => (
-  <li key = {`char-${index}`} className = "card">
-    <p>{char.name} </p>
-    <p> index {index}</p>
-    <p> {char.height}</p>
-    <p> {char.mass}</p>
-  </li>
-))
-
-
-  return (
-    <div className="App">
-      <ul> 
-      {characterList}
-      </ul>
+// function App() {
+//   interface card {
+//     name: string;
+//     height: number;
+//     mass: number;
+//   }
+//   const [characters, setCharacters] = useState<card[]>([])
+//   useEffect(() => {
+//     fetch("https://swapi.dev/api/people")
+//     .then(res => res.json())
+//     .then(char => { setCharacters(char.results)})
+//     .catch(err => console.log(err))
+//   },[]) 
+// // console.log(characters)
+// //can access array index from second parameter of map function
+// const characterList = characters.map((char,index) => (
+//   <li key = {`char-${index}`} className = "card">
+//     <p>{char.name} </p>
+//     <p> index {index}</p>
+//     <p> {char.height}</p>
+//     <p> {char.mass}</p>
+//   </li>
+// ))
+//   return (
+//     <div className="App">
+//       <ul> 
+//       {characterList}
+//       </ul>
       
-    </div>
-  )
-}
+//     </div>
+//   )
+// }
 
-export default App
+
+
+
+ export default App
+
+
